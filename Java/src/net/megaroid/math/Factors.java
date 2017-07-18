9971d9562369f2e4b493e91e692738a0de5b08cc
xmenta <xmentak@gmail.com>
2017-07-18 23:29:55 +0900

    Add printAllFactors() method

--- Java/src/net/megaroid/math/Factors.java
+++ Java/src/net/megaroid/math/Factors.java
@@ -12,10 +12,25 @@ public class Factors {
 		return mNumber;
 	}
 	
+	public void printAllFactors() {
+		System.out.print("Factors: ");
+		int sqrtN = (int)Math.sqrt(mNumber);
+		for (int i = 1; i <= sqrtN; i++) {
+			if (mNumber % i == 0) {
+				System.out.print(i + " ");
+				if (i * i != mNumber) {
+					System.out.print((mNumber / i) + " ");
+				}
+			}
+		}
+		System.out.println("");
+	}
+	
 	public static void main(String[] args) {
 		
 		Factors f = new Factors(10);
 		System.out.println("The number is " + f.getNumber());
 		
+		f.printAllFactors();
 	}
 }
