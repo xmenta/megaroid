2b83d3e659379e9dbe24f686e8a47a1568d98af9
xmenta <xmentak@gmail.com>
2017-07-18 21:48:22 +0900

    Add main method.

--- Java/src/net/megaroid/math/Factors.java
+++ Java/src/net/megaroid/math/Factors.java
@@ -11,4 +11,11 @@ public class Factors {
 	public int getNumber() {
 		return mNumber;
 	}
+	
+	public static void main(String[] args) {
+		
+		Factors f = new Factors(10);
+		System.out.println("The number is " + f.getNumber());
+		
+	}
 }
